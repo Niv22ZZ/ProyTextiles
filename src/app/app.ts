ee008be42ee10a294eee50ab2ca9ea55c9d816ff
Kevin <nivekzerres123@gmail.com>
2025-08-22 21:12:54 -0400

    Agregar imagenes con Angular Material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatGridListModule ,MatCardModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
+
 })
 export class App {
-  protected readonly title = signal('ProyTextiles');
+  protected readonly title = signal('Modelos');
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 }
